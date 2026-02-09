9850b11db592c95faefc25d5bfefd00e27036eb4
nousra0 <raissouninossra@gmail.com>
2026-02-09 10:52:02 +0300

    fix: add default export to bootstrap plugin

--- plugins/bootstrap.client.ts
+++ plugins/bootstrap.client.ts
@@ -1,2 +1,6 @@
 // Bootstrap 5 JS (optional: for dropdowns, modals). Import only on client.
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+export default defineNuxtPlugin(() => {
+  // Bootstrap is loaded via side-effect import above
+})
